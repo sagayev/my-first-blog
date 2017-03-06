71e1e41504e0fbbd2846a2fafafda1eabd232ae0
Samir <sagayev@gmail.com>
2017-03-06 11:08:44 +0400

    Added comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from .models import Post
+from .models import Comment
 
 # Register your models here.
-from .models import Post
+
 admin.site.register(Post)
+admin.site.register(Comment)
